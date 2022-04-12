9f082406a9562dbd76aed541a9d10ba982093667
AdamG682 <adamgiles@u.boisestate.edu>
2022-04-11 19:58:44 -0600

    final updates for demo

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  goBack(){
+    this.router.navigate(['/tabs/tab1']);
   }
 
 }
